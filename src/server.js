b1f43d1736d84f5018b8cfeeb184ce7591a918a8
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2026-03-09 21:08:08 +0200

    feat(notes): add endpoint to retrieve note by id

--- src/server.js
+++ src/server.js
@@ -18,7 +18,8 @@ app.use(
         colorize: true,
         translateTime: 'HH:MM:ss',
         ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
@@ -27,6 +28,11 @@ app.use(
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
 app.listen(PORT, () => {
