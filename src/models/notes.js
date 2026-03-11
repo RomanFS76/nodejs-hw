aa7df979b8e9edbaab8fac4de7c8b5dab838542e
RomanFS76 <korvy85@gmail.com>
2026-03-11 15:30:54 +0300

    refactor: replace Student model with Note model

---
+++ src/models/notes.js
@@ -0,0 +1,38 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const notesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    tag: {
+      type: Number,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', notesSchema);
