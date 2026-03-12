e6f602a3be7490b1013bdd3aa3eb9b74506c3039
RomanFS76 <korvy85@gmail.com>
2026-03-12 17:41:36 +0300

    feat(controllers); add controllers to routes

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { Note } from './models/notes.js';
+// import { Note } from './models/notes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -13,27 +15,13 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-app.get('/notes', async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
-});
+app.use(notesRoutes);
 
-app.get('/notes/:noteId', async (req, res) => {
-  const { noteId } = req.params;
-  const note = await Note.findById(noteId);
-  if (!note) {
-    return res.status(404).json({ message: 'Note not found' });
-  }
-  res.status(200).json(note);
-});
+// app.get('/test-error', () => {
+//   throw new Error('Simulated server error');
+// });
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+app.use(notFoundHandler);
 
 app.use(errorHandler);
 
