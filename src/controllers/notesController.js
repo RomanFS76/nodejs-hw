c4c9a59091ffb1478725412be96dd4f1d3b9bc15
RomanFS76 <korvy85@gmail.com>
2026-03-14 10:54:44 +0300

    feat(notes): add deleteNote controller rename controllers

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,12 +1,12 @@
 import { Note } from '../models/notes.js';
 import createHttpError from 'http-errors';
 
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
 
-export const getNotesById = async (req, res) => {
+export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
   if (!note) {
@@ -18,4 +18,15 @@ export const getNotesById = async (req, res) => {
 export const createNote = async (req, res) => {
   const note = await Note.create(req.body);
   res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+  res.status(200).json(note);
 };
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import {
   createNote,
-  getNotes,
-  getNotesById,
+  deleteNote,
+  getAllNotes,
+  getNoteById,
 } from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get('/notes', getNotes);
+router.get('/notes', getAllNotes);
 
-router.get('/notes/:noteId', getNotesById);
+router.get('/notes/:noteId', getNoteById);
 
 router.post('/notes', createNote);
+
+router.delete('/notes/:noteId', deleteNote);
 
 export default router;
