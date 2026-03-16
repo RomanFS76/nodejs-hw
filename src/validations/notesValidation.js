137b31959e278803aca91cfd5a52dd751f631470
RomanFS76 <korvy85@gmail.com>
2026-03-16 15:20:20 +0300

    feat(constants): add tags for notes

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,9 @@
+// import { Joi, Segments } from 'celebrate';
+
+// export const getAllNotesSchema = {
+//   [Segments.BODY]: Joi.object({
+//     title: Joi.string().min(3).max(30).required(),
+//     content: Joi.string().max(65),
+//     tag: Joi.string().valid('male', 'female', 'other').required(),
+//   }),
+// };
