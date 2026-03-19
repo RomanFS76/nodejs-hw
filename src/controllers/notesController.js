9d7b02432799a9633fc0cb6a8a5fe4d84095898f
RomanFS76 <korvy85@gmail.com>
2026-03-19 15:04:13 +0300

    refactor(notes): update contorllers, add userId

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,13 +2,13 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
-  const { page = 1, perPage = 10, tag, search  } = req.query;
+  const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1) * perPage;
 
   const notesQuery = Note.find();
 
   if (tag) {
-    notesQuery.where("tag").equals(tag);
+    notesQuery.where('tag').equals(tag);
   }
 
   if (search) {
@@ -26,7 +26,10 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById(noteId);
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: req.user._id,
+  });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
@@ -41,7 +44,8 @@ export const createNote = async (req, res) => {
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndDelete({
-    _id: noteId,
+    id: noteId,
+    userId: req.user._id,
   });
   if (!note) {
     throw createHttpError(404, 'Note not found');
@@ -51,7 +55,7 @@ export const deleteNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate({ _id: noteId }, req.body, {
+  const note = await Note.findByIdAndUpdate({ _id: noteId, userId: req.user._id }, req.body, {
     returnDocument: 'after',
   });
   if (!note) {
