1de6930c3b71cd179ecfa2856b7719277fca3a05
RomanFS76 <korvy85@gmail.com>
2026-03-19 12:03:11 +0300

    feat(auth): add cookie handling for accessToken, refreshToken and sessionId

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -16,6 +17,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+app.use(cookieParser());
 
 app.use(notesRoutes);
 app.use(authRoutes);
