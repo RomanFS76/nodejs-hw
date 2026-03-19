4215b3f761f3a554b09059cd3dfa6f82705f95f3
RomanFS76 <korvy85@gmail.com>
2026-03-19 10:56:56 +0300

    feat(auth): add login controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
 
 export const registerUser = async (req, res) => {
@@ -11,9 +11,23 @@ export const registerUser = async (req, res) => {
     throw createHttpError(400, 'Email in use');
   }
 
-  const hashedPassword = await bcrypt.hash (password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = await User.create({ email, password: hashedPassword });
 
-  res.status(201).json({newUser});
+  res.status(201).json({ newUser });
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+  res.status(200).json(user);
 };
