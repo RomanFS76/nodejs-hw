30074e6c52ac17c4901dc7bf45db1d265ae00512
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2026-03-30 21:50:55 +0300

    feat: add password reset token generation and email sending

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendEmail.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -93,11 +95,34 @@ export const requestResetEmail = async (req, res) => {
   const { email } = req.body;
 
   const user = await User.findOne({ email });
+
   if (!user) {
-    throw createHttpError(404, 'User not found');
+    res.status(200).json({
+      message: 'Password reset email sent successfully',
+    });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    });
+  } catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
   }
 
   res.status(200).json({
-    message: 'Password reset email sent successfully',
+    message: 'If this email exists, a reset link has been sent',
   });
 };
