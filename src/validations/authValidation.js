e123354485c3236df8b22e4df98caaf7061c8b26
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2026-03-30 19:26:09 +0300

    feat(auth):  add request reset email validation schema

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -14,3 +14,8 @@ export const  loginUserSchema = {
     password: Joi.string().required(),
   })
 };
+ export const requestResetEmailSchema = {
+  [Segments.BODY] : Joi.object({
+    email: Joi.string().email().required(),
+  })
+};
