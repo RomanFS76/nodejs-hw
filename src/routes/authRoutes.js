bde455ce91bb13a528e1c557041885a210af7e90
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2026-03-30 19:31:17 +0300

    feat(auth): add router for password reset email request

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser,loginUser,logoutUser,refreshUserSession } from '../controllers/authController.js';
-import { registerUserSchema,loginUserSchema } from '../validations/authValidation.js';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  requestResetEmail
+} from '../controllers/authController.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+  requestResetEmailSchema,
+} from '../validations/authValidation.js';
 
 const router = Router();
 
@@ -9,5 +19,10 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 
 export default router;
